7d37ce5039f902c5b1d3ad3dce8860d5d3e86023
Jonathan Fischer Friberg <odyssomay@gmail.com>
2012-05-05 00:37:05 +0200

    internals listen to hash

--- src/app/internals.cljs
+++ src/app/internals.cljs
@@ -1,22 +1,34 @@
 (ns internals)
 
-(js/$ (fn []
-        (.each (js/$ ".link-internal")
-               (fn [i e] 
-                 (let [j-e (js/$ e)
-                       target (.attr j-e "target_internal")
-                       target-container (.attr j-e "target_container")]
-                   (if target
-                     (.click j-e (fn []
-                                   (let [c (if target-container target-container "internals-container")
-                                         j-c (js/$ (str "#" c))]
-                                     (.get js/$ (str "/internal/" target)
-                                           (fn [data]
-                                             (doto j-c
-                                               (.empty)
-                                               (.hide)
-                                               (.append data)
-                                               (.fadeIn 50)
-                                               ))))))))))))
+(def $ (js/$))
+
+(defn render-internal [internal container]
+  (let [c (if container container "internals-container")
+        j-c (js/$ (str "#" c))]
+    (.get js/$ (str "/internal/" internal)
+          (fn [data]
+            (doto j-c
+              (.empty)
+              (.hide)
+              (.append data)
+              (.fadeIn 50)
+              )))))
+
+($ (fn []
+     (.hashchange ($ js/window)
+                  (fn [] 
+                    (let [h (.-hash (.-location window))]
+                      (render-internal (.substring h 1) nil)
+                      )))
+
+     (.each (js/$ ".link-internal")
+            (fn [i e] 
+              (let [j-e (js/$ e)
+                    target (.attr j-e "target_internal")
+                    target-container (.attr j-e "target_container")]
+                (if target
+                  (.click j-e (fn []
+                                (render-internal target target_container)))))))
+     ))
 
 
