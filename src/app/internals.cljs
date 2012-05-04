c3fd635661a8efc07a301da79518a924e92a7d8e
Jonathan Fischer Friberg <odyssomay@gmail.com>
2012-05-05 00:41:04 +0200

    fixes

--- src/app/internals.cljs
+++ src/app/internals.cljs
@@ -1,11 +1,12 @@
 (ns internals)
 
 (def $ (js/$))
+(def window (js* "window"))
 
 (defn render-internal [internal container]
   (let [c (if container container "internals-container")
-        j-c (js/$ (str "#" c))]
-    (.get js/$ (str "/internal/" internal)
+        j-c ($ (str "#" c))]
+    (.get $ (str "/internal/" internal)
           (fn [data]
             (doto j-c
               (.empty)
@@ -15,15 +16,15 @@
               )))))
 
 ($ (fn []
-     (.hashchange ($ js/window)
+     (.hashchange ($ window)
                   (fn [] 
                     (let [h (.-hash (.-location window))]
                       (render-internal (.substring h 1) nil)
                       )))
 
-     (.each (js/$ ".link-internal")
+     (.each ($ ".link-internal")
             (fn [i e] 
-              (let [j-e (js/$ e)
+              (let [j-e ($ e)
                     target (.attr j-e "target_internal")
                     target-container (.attr j-e "target_container")]
                 (if target
